b17e4549dea3fac50a2fdcc7a897549ee41f6562
Dmitry_Smirnov <dimacm14@gmail.com>
2023-07-11 15:39:20 +0300

    Corrected ArrayStorage

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private static final int STORAGE_LIMIT = 10000;
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int countResume;
 
     public void clear() {
@@ -18,34 +19,41 @@ public class ArrayStorage {
 
     public void update(Resume resume) {
         String uuid = resume.getUuid();
-        int place = findPlace(uuid);
-        if (isExists(uuid, place)) {
-            storage[place] = resume;
+        int index = findIndex(uuid);
+        if (isExists(index)) {
+            storage[index] = resume;
+        } else {
+            System.out.printf("ERROR: resume with uuid '%s' does not exist%n", uuid);
         }
     }
 
     public void save(Resume resume) {
+        String uuid = resume.getUuid();
         if (countResume == storage.length) {
             System.out.println("ERROR: storage is full");
+        } else if (isExists(findIndex(uuid))) {
+            System.out.printf("ERROR: resume with uuid '%s' already exists%n", uuid);
         } else {
-            String uuid = resume.getUuid();
-            if (findPlace(uuid) >= 0) {
-                System.out.printf("ERROR: resume with uuid '%s' already exists%n", uuid);
-            } else {
-                storage[countResume++] = resume;
-            }
+            storage[countResume++] = resume;
         }
     }
 
     public Resume get(String uuid) {
-        int place = findPlace(uuid);
-        return (isExists(uuid, place) ? storage[place] : null);
+        int index = findIndex(uuid);
+        if (isExists(index)) {
+            return storage[index];
+        } else {
+            System.out.printf("ERROR: resume with uuid '%s' does not exist%n", uuid);
+            return null;
+        }
     }
 
     public void delete(String uuid) {
-        int place = findPlace(uuid);
-        if (isExists(uuid, place)) {
-            System.arraycopy(storage, place + 1, storage, place, countResume-- - place - 1);
+        int index = findIndex(uuid);
+        if (isExists(index)) {
+            System.arraycopy(storage, index + 1, storage, index, countResume-- - index - 1);
+        } else {
+            System.out.printf("ERROR: resume with uuid '%s' does not exist%n", uuid);
         }
     }
 
@@ -60,7 +68,7 @@ public class ArrayStorage {
         return countResume;
     }
 
-    private int findPlace(String uuid) {
+    private int findIndex(String uuid) {
         for (int i = 0; i < countResume; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
@@ -69,11 +77,7 @@ public class ArrayStorage {
         return -1;
     }
 
-    private boolean isExists(String uuid, int place) {
-        if (place < 0) {
-            System.out.printf("ERROR: resume with uuid '%s' does not exist%n", uuid);
-            return false;
-        }
-        return true;
+    private boolean isExists(int index) {
+        return (index >= 0);
     }
 }
