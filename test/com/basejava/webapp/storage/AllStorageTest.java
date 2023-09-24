7d4c9dfd5d5d1889c0851c9b6dd9a3fb176ce647
Dmitry_Smirnov <dimacm14@gmail.com>
2023-09-25 01:00:48 +0300

    Added tests to AllStorageTest

--- test/com/basejava/webapp/storage/AllStorageTest.java
+++ test/com/basejava/webapp/storage/AllStorageTest.java
@@ -9,7 +9,10 @@ import org.junit.runners.Suite;
         SortedArrayStorageTest.class,
         ListStorageTest.class,
         MapUuidStorageTest.class,
-        MapResumeStorageTest.class
+        MapResumeStorageTest.class,
+        ObjectStreamFileStorageTest.class,
+        ObjectStreamPathStorageTest.class
+
 })
 public class AllStorageTest {
 }
