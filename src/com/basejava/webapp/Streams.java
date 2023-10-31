dcf4530bc7a12b50432f00bb9c07a1abb3ee6cd2
Dmitry_Smirnov <javatest9k6k5@gmail.com>
2023-10-31 19:21:08 +0300

    Add minValue and oddOrEven methods

--- src/com/basejava/webapp/Streams.java
+++ src/com/basejava/webapp/Streams.java
@@ -7,14 +7,19 @@ import static java.util.stream.Collectors.*;
 public class Streams {
 
     public static void main(String[] args) {
-        // minValue
         int[] ints = new int[] {1, 2, 3, 3, 2, 3};
-        int minValue = Arrays.stream(ints).distinct().sorted().reduce((x, y) -> x * 10 + y).getAsInt();
-        System.out.println(minValue);
+        System.out.println(minValue(ints));
 
-        // oddOrEven
         List<Integer> integers = new ArrayList<>(List.of(1, 1, 1, 2, 2, 1, 3));
+        System.out.println(oddOrEven(integers));
+    }
+
+    public static int minValue(int[] values) {
+        return Arrays.stream(values).distinct().sorted().reduce((x, y) -> x * 10 + y).getAsInt();
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
         Map<Boolean, List<Integer>> oddAndEven = integers.stream().collect(partitioningBy(x -> x % 2 == 0, toList()));
-        System.out.println(oddAndEven.get(oddAndEven.get(false).size() % 2 != 0));
+        return oddAndEven.get(oddAndEven.get(false).size() % 2 != 0);
     }
 }
