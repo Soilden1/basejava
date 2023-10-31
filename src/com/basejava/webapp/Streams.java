effe051989ccec446884bd18f826437ad1658627
Dmitry_Smirnov <javatest9k6k5@gmail.com>
2023-10-31 19:15:30 +0300

    Add comments

--- src/com/basejava/webapp/Streams.java
+++ src/com/basejava/webapp/Streams.java
@@ -7,10 +7,12 @@ import static java.util.stream.Collectors.*;
 public class Streams {
 
     public static void main(String[] args) {
+        // minValue
         int[] ints = new int[] {1, 2, 3, 3, 2, 3};
         int result = Arrays.stream(ints).distinct().sorted().reduce((x, y) -> x * 10 + y).getAsInt();
         System.out.println(result);
 
+        // oddOrEven
         List<Integer> integers = new ArrayList<>(List.of(1, 1, 1, 2, 2, 1, 3));
         Map<Boolean, List<Integer>> oddAndEven = integers.stream().collect(partitioningBy(x -> x % 2 == 0, toList()));
         System.out.println(oddAndEven.get(oddAndEven.get(false).size() % 2 != 0));
